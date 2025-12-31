ad0634c76d6b867b365d0005d8a848c1dbbfebee
Kp1303 <143245424+KashishPundir@users.noreply.github.com>
2025-12-31 23:14:43 +0530

    Demo Input file
    Added a comment for data loading section.

--- demo/Input file.py
+++ demo/Input file.py
@@ -1,5 +1,6 @@
 from cognia import eda_report
 import pandas as pd
-df=pd.read_csv(r"c:/Users/Kashish Pundir/Downloads/Datasets/laptopPrice.csv")
 
+# Data Loading
+df=pd.read_csv(r"c:/Users/Kashish Pundir/Downloads/Datasets/laptopPrice.csv")
 eda_report(df)
